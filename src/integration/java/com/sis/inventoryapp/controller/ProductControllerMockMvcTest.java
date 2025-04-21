9c98cbc7221cb6a319fd5df322a63df3e4027176
Damodar Das <dinesh.k.kaushish@gmail.com>
2025-04-21 15:20:27 +0100

    Adds initial integration test logic.

--- src/integration/java/com/sis/inventoryapp/controller/ProductControllerMockMvcTest.java
+++ src/integration/java/com/sis/inventoryapp/controller/ProductControllerMockMvcTest.java
@@ -1,16 +1,96 @@
 package com.sis.inventoryapp.controller;
 
+import com.sis.inventoryapp.domain.Product;
+import com.sis.inventoryapp.service.ProductService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductController.class)
 class ProductControllerMockMvcTest {
 
-    @Test
-    void getAllProducts() {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService mockProductService;
+
+    private static Product product;
+    private static Product product1;
+    private static Product product2;
+    private static List<Product> productList;
+
+    @BeforeAll
+    static void setup(){
+        product = new Product("Aged Brie", 1, 1);
+        product1 = new Product("Backstage Passes", 1, 1);
+        product2 = new Product("Backstage Passes", 1, 1);
+
+        productList = new ArrayList<>();
+        productList.add(product);
+        productList.add(product1);
+        productList.add(product2);
     }
 
     @Test
-    void addProducts() {
+    public void shouldReturnProductList_whenFetchingAllProductsSuccess() throws Exception {
+        when(mockProductService.getAllProducts()).thenReturn(productList);
+
+        this.mockMvc.perform(get("/products"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].name").value("Aged Brie"))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].sellInDate").value(1))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].Quality").value(1))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].name").value("Backstage Passes"))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].sellInDate").value("-1"))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].quality").value("2"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].name").value("Backstage Passes"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].sellInDate").value("9"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].quality").value("2"))
+                .andExpect((ResultMatcher) jsonPath("$.product.length()").value(3));
+    }
+
+    @Test
+    void shouldReturnProductsList_whenProductsAddedToDbSuccess() throws Exception {
+        String jsonInput = "[{\"name\":\"AgedBrie\",\"sell_indate\":1\"quality\":1},{\"name\":\"BackstagePasses\",\"sell_indate\":1\"quality\":2}," +
+                                "{\"name\":\"BackstagePasses\",\"sell_indate\":9\"quality\":2}]";
+
+        when(mockProductService.addProducts(anyList())).thenReturn(productList);
+        this.mockMvc.perform(post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonInput)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].name").value("Aged Brie"))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].sellInDate").value(1))
+                .andExpect((ResultMatcher) jsonPath("$.product[0].quality").value(1))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].name").value("Backstage Passes"))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].sellInDate").value("-1"))
+                .andExpect((ResultMatcher) jsonPath("$.product[1].quality").value("2"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].name").value("Backstage Passes"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].sellInDate").value("9"))
+                .andExpect((ResultMatcher) jsonPath("$.product[2].quality").value("2"))
+                .andExpect((ResultMatcher) jsonPath("$.product.length()").value(3));
     }
 }
--- src/main/java/com/sis/inventoryapp/domain/Product.java
+++ src/main/java/com/sis/inventoryapp/domain/Product.java
@@ -1,4 +1,41 @@
 package com.sis.inventoryapp.domain;
 
+import java.util.Objects;
+
 public class Product {
+
+    private String name;
+    private int sellInDate;
+    private int Quality;
+
+    public Product(String name, int sellInDate, int quality) {
+        this.name = name;
+        this.sellInDate = sellInDate;
+        Quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellInDate() {
+        return sellInDate;
+    }
+
+    public int getQuality() {
+        return Quality;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return sellInDate == product.sellInDate && Quality == product.Quality && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellInDate, Quality);
+    }
 }
